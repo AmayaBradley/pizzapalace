f209cbc856067b0b3ceb3f92d4073f03e2bedd5b
unknown <AmayaBradley>
2019-10-06 23:40:13 +0300

    Declared variables for the input and converted them to Interger data types.This allows them to be used in mathematical operations .
    q

---
+++ style.js
@@ -0,0 +1,54 @@
+function getTotal(){
+var esize = parseInt(document.getElementById("size").value);
+var ecrust = parseInt(document.getElementById("crust").value);
+var etop = parseInt(document.getElementById("toppings").value);
+var eamt = parseInt(document.getElementById("amt").value);
+
+
+
+
+var asize = parseInt(document.getElementById("size1").value);
+var acrust = parseInt(document.getElementById("crust1").value);
+var atop = parseInt(document.getElementById("toppings1").value);
+var aamt = parseInt(document.getElementById("amt1").value);
+
+
+var chickenSize = parseInt(document.getElementById("size2").value);
+var chickenCrust = parseInt(document.getElementById("crust2").value);
+var chickenTop = parseInt(document.getElementById("toppings2").value);
+var chickenAmt = parseInt(document.getElementById("amt2").value);
+
+
+var chitikaSize = parseInt(document.getElementById("size3").value);
+var chitikaCrust = parseInt(document.getElementById("crust3").value);
+var chitikaTop = parseInt(document.getElementById("toppings3").value);
+var chitikaAmt = parseInt(document.getElementById("amt3").value);
+
+
+var creamySize = parseInt(document.getElementById("size4").value);
+var creamyCrust = parseInt(document.getElementById("crust4").value);
+var creamyTop = parseInt(document.getElementById("toppings4").value);
+var creamyAmt = parseInt(document.getElementById("amt4").value);
+
+
+var hawSize = parseInt(document.getElementById("size5").value);
+var hawCrust = parseInt(document.getElementById("crust5").value);
+var hawTop = parseInt(document.getElementById("toppings5").value);
+var hawAmt = parseInt(document.getElementById("amt5").value);
+
+
+var marSize = parseInt(document.getElementById("size6").value);
+var marCrust = parseInt(document.getElementById("crust6").value);
+var marTop = parseInt(document.getElementById("toppings6").value);
+var marAmt = parseInt(document.getElementById("amt6").value);
+
+
+var meaSize = parseInt(document.getElementById("size7").value);
+var meaCrust = parseInt(document.getElementById("crust7").value);
+var meaTop = parseInt(document.getElementById("toppings7").value);
+var meaAmt = parseInt(document.getElementById("amt7").value);
+
+
+
+var finalAmt = 
+}
